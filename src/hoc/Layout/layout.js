881264cfb040c7bcf35b15d6d273cdf6a4b9e491
cgultunca <info@cemgultunca.com.tr>
2019-05-05 15:19:13 +0100

    components added

---
+++ src/hoc/Layout/layout.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './layout.css'
+export default class Layout extends Component {
+
+    state = {
+
+    }
+
+
+  render() {
+    return (
+      <div>
+        Header
+        {this.props.children}
+        footer 
+      </div>
+    )
+  }
+}
